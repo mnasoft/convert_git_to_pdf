9338756b71c22fe83052f7cdf8581f6da87a6c9a
Mykola Matvyeyev <mnasoft@gmail.com>
2024-11-13 21:58:01 +0200

    Ср 13 ноя 2024 19:58:01 UTC

--- gif_to_pdf.sh
+++ gif_to_pdf.sh
@@ -32,16 +32,23 @@ fi
 # Параметры:
 BASE_DIR="$1"
 
-# Функция для проверки использования оперативной памяти
-check_memory_usage (){
-    local memory_usage=$(free | awk '/Mem/ {printf "%.0f", $3/$2 * 100.0}')
-    while [ "$memory_usage" -ge 30 ]
+create_pdf () {
+    local cdir=`pwd`
+    local dir=$1
+    cd ${dir}
+
+    # Подсчет количества файлов с расширением .gif
+    count=$(find "$dir" -type f -name "*.gif" | wc -l)
+    
+    echo "Количество файлов с расширением .gif в каталоге ${dir}: ${count}"
+
+    for i in `find . -name "*.gif" | sort -V`
     do
-        echo "Использование памяти более чем на 30% ($memory_usage%). Ожидание 2 секунды..."
-        sleep 2
-        memory_usage=$(free | awk '/Mem/ {printf "%.0f", $3/$2 * 100.0}')
-        echo "Текущая загрузка памяти: $memory_usage%"
+        magick $i $i.pdf
     done
+    qpdf --empty --pages *.gif.pdf -- gost.pdf
+    rm *.gif.pdf
+    cd ${cdir}
 }
 
 # Функция для обработки подкаталогов
@@ -54,10 +61,9 @@ process_directory() {
         # Проверка наличия файла gost.pdf
         if [ ! -f "$dir/gost.pdf" ]
         then
-            # Проверка использования памяти
             # check_memory_usage
             # Если файла gost.pdf нет, выполняем команду
-            magick -limit memory 1GB `ls -v "$dir"/*.gif` "$dir/gost.pdf"
+            create_pdf "$dir"
             echo "Создан файл gost.pdf в каталоге: $dir"
         else
             echo "Файл gost.pdf уже существует в каталоге: $dir"
@@ -66,7 +72,7 @@ process_directory() {
 }
 
 # Обход подкаталогов
-find "$BASE_DIR" -type d | sort -Vr | while read -r subdir
+find "$BASE_DIR" -type d | sort -V | while read -r subdir
 do
   process_directory "$subdir"
 done
